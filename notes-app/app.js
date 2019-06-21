36ee38e271770de868e9301f650fc775abab50bb
github username <jonathansychan@gmail.com>
2019-06-21 01:07:17 -0400

    Lecture 12

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,22 @@
+const getNotes = require('./notes.js')
+const chalk = require(`chalk`)
+
+const msg = getNotes()
+console.log(msg)
+
+console.log(chalk.green("Success!"))
+
+console.log(chalk.bold.blue.inverse.underline("testing"))
+
+
+
+
 // module exports is what is the return value of require.
 // const add = require(`./Extra/utils2.js`)
 //
 // const sum = add(4, -2)
 // console.log(sum)
-const validator = require(`validator`)
-const getNotes = require('./notes.js')
-
-const msg = getNotes()
-
-console.log(msg)
+// const validator = require(`validator`)
 
 // using the validator library that was imported from npm
-console.log(validator.isURL('https://jonathansychan.com'))
+// console.log(validator.isURL('https://jonathansychan.com'))
