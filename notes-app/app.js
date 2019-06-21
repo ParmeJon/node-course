77a46b7f2e10dc09788f7fb234e479f17a7696cb
github username <jonathansychan@gmail.com>
2019-06-21 00:48:34 -0400

    adds validator package & continues with Lecture 11

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,9 +3,12 @@
 //
 // const sum = add(4, -2)
 // console.log(sum)
-
+const validator = require(`validator`)
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+// using the validator library that was imported from npm
+console.log(validator.isURL('https://jonathansychan.com'))
