484a7ed3dff164532cbc2df3e9448c64029648f1
github username <jonathansychan@gmail.com>
2019-06-24 00:52:06 -0400

    adds yarg package to handle commands with process.argv (lecture20)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,83 @@
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
+const yargs = require('yargs')
 const chalk = require(`chalk`)
 
-const msg = getNotes()
-console.log(msg)
+// customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.green("Success!"))
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Description',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
 
-console.log(chalk.bold.yellow.inverse.underline("Error!"))
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title to remove.',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing all notes')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read your notes',
+  handler: function () {
+    console.log('Reading a note')
+  }
+})
+
+
+yargs.parse()
+// console.log(yargs.argv)
+
+// if (command === 'add') {
+//   console.log('Adding note!')
+// } else if (command === 'remove') {
+//   console.log('Removing note!')
+// }
 
 
 
+// const msg = getNotes()
+// console.log(msg)
+//
+// console.log(chalk.bold.blue.inverse.underline("Success!"))
+//
+// console.log(process.argv[2])
 
 // module exports is what is the return value of require.
 // const add = require(`./Extra/utils2.js`)
