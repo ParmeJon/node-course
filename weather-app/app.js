92187474abbf458f034afd7f9ae8c10bf7de9d7c
github username <jonathansychan@gmail.com>
2019-06-25 19:00:03 -0400

    destructured weatherapp && LECTURE 42

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,25 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-require('dotenv').config() 
+require('dotenv').config()
 
 const address = process.argv[2]
 
 if (!address) {
   console.log('Please provide an address')
 } else {
-  geocode(address, (error, data) => {
+  geocode(address, (error, {latitude: dataLat, longitude: dataLong, location}) => {
     if (error) {
       return console.log(error)
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(dataLat, dataLong, (error, { answer }) => {
       if (error) {
         return console.log(error)
       }
 
-      console.log(data.location)
-      console.log(forecastData.answer)
+      console.log(location)
+      console.log(answer)
     })
   })
 }
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 const forecast = (lat, long, callback) => {
   const url = `https://api.darksky.net/forecast/${process.env.DB_DARKSKYTOKEN}/${lat},${long}`
 
-  request({ url: url, json: true}, (error, response) => {
+  request({ url, json: true}, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather services', undefined)
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
       callback(undefined, {
-        answer: `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} out. There is a ${response.body.currently.precipProbability}% chance of rain.`
+        answer: `${body.daily.data[0].summary} It is currently ${body.currently.temperature} out. There is a ${body.currently.precipProbability}% chance of rain.`
       })
     }
   })
