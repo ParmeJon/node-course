5e6d76a0449665e439684dc1dc6c806521cf9531
github username <jonathansychan@gmail.com>
2019-06-25 18:30:04 -0400

    destructuring

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -11,13 +11,30 @@ const user = {
 
 console.log(user)
 
-// Object destructuring
+// Object Destructuring
 
 const product = {
   label: 'Red notebook',
   price: 3,
   stock: 201,
-  salePrice: undefined
+  salePrice: undefined,
+  rating: 4.2
 }
 
+// manual way
+// const label = product.label
+// const stock = product.stock
+
 // extract obj properties into individual variables
+// You can also rename variables with destructuring
+// You can also set up a default value if there isn't a property
+
+const {label: productLabel, stock, rating = 5} = product
+console.log(productLabel, stock, rating)
+
+// Can destructure inline!
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock)
+}
+
+transaction('order', product)
