77a458fb7b09e1bde0320050b618f696bd2f2f2b
github username <jonathansychan@gmail.com>
2019-07-07 18:47:38 -0400

    sets up task-manager models and a few routes

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,50 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
 
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String,
-//     required: true,
-//     trim: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//     lowercase: true,
-//     validate(value) {
-//       if (!validator.isEmail(value)) {
-//         throw new Error('Email is invalid.')
-//       }
-//     }
-//   },
-//   age: {
-//     type: Number,
-//     default: 0,
-//     validate(value) {
-//       if (value < 0) {
-//         throw new Error('Age must be a positive number.')
-//       }
-//     }
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     minLength: 7,
-//     trime: true,
-//     validate(value) {
-//       if (value.toLowerCase().includes("password")) {
-//         throw new Error('Password must not include password')
-//       }
-//     }
-//   }
-// })
-//
 // const me = new User({
 //   name: '  Andrew  ',
 //   email: '  jon@chan.io  ',
@@ -58,25 +18,12 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
 //   console.log('Error!', error)
 // })
 
-// converts lowercase and pluralizes model name in mongoDB
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
-
-const cry = new Task({
-  description: '   Excreting salt water from eyes.    '
-})
-
-cry.save().then((cry) => {
-  console.log(cry)
-}).catch((error) => {
-  console.log('Error!', error)
-})
+// const cry = new Task({
+//   description: '   Excreting salt water from eyes.    '
+// })
+//
+// cry.save().then((cry) => {
+//   console.log(cry)
+// }).catch((error) => {
+//   console.log('Error!', error)
+// })
