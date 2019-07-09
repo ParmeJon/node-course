466e05010b61115799a98caa51d4a04a9940a426
github username <jonathansychan@gmail.com>
2019-07-08 20:24:03 -0400

    separates routers and adds full CRUD for tasks and users

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -2,11 +2,24 @@ require('../src/db/mongoose')
 const User = require('../src/models/user')
 
 // mongoose takes care of the $set syntax in mongoDB
-User.findByIdAndUpdate('5d1f71f50ed0a27132727f5d', { age: 1 }).then((user) => {
-  console.log(user)
-  return User.countDocuments({ age: 1 })
-}).then((result) => {
-  console.log(result)
+// User.findByIdAndUpdate('5d1f71f50ed0a27132727f5d', { age: 1 }).then((user) => {
+//   console.log(user)
+//   return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//   console.log(result)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+
+  return count
+}
+
+updateAgeAndCount('5d1f71f50ed0a27132727f5d', 2).then((count) => {
+  console.log(count)
 }).catch((e) => {
   console.log(e)
 })
