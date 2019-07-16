fd688b2f376af83ac09c334c947ae9dff567f264
github username <jonathansychan@gmail.com>
2019-07-16 17:19:01 -0400

    Adds avatar delete route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -111,8 +111,8 @@ router.delete('/users/me', auth, async (req, res) => {
 
 
 // File Uploads
+// without dest property, multer will pass to req for our access to the file instead.
 const upload = multer({
-  dest: 'avatar',
   limits: {
     fileSize: 1000000
   },
@@ -124,10 +124,20 @@ const upload = multer({
   }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  // buffer of binary data
+  req.user.avatar = req.file.buffer
+  await req.user.save()
   res.send()
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message })
+})
+
+// Delete Avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
 })
 
 
