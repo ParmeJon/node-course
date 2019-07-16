249b066169a58195d046dca007560256c4c9ba1c
github username <jonathansychan@gmail.com>
2019-07-16 16:51:41 -0400

    Adds validations for multer requests

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -112,7 +112,16 @@ router.delete('/users/me', auth, async (req, res) => {
 
 // File Uploads
 const upload = multer({
-  dest: 'avatar'
+  dest: 'avatar',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please provide a jpg, jpeg or png file.'))
+    }
+    cb(undefined, true)
+  }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
