c062d3756dbc838378877dfdb8bb59085e9a18c5
github username <jonathansychan@gmail.com>
2019-07-20 18:00:28 -0400

    Sets up css and starts room features.

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -17,8 +17,17 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('New websocket connection.')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit("message", generateMessage("Welcome!"));
+        socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined!`));
+
+
+        // socket.emit, io.emit, socket.broadcast.emit
+        // io.to.emit -everybody in a room 
+        // socket.broadcast.to.emit - everybody except client in room
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -27,7 +36,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', generateMessage(message))
+        io.to('Center City').emit('message', generateMessage(message))
         callback()
     })
 
