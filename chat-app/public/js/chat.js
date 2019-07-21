63e2c986e0c62bd943766a57fb604077f77de1a4
github username <jonathansychan@gmail.com>
2019-07-20 23:05:43 -0400

    Sets up auto scrolling function

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,27 +10,68 @@ const $messages = document.querySelector('#messages')
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options 
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoscroll = () => {
+    // new message element
+    const $newMessage = $messages.lastElementChild
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+    
+    // Visible height 
+    const visibleHeight = $messages.offsetHeight
+    
+    // Height of messages container 
+    const containerHeight = $messages.scrollHeight
+    
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+    
+    console.log('messages scroll Top', $messages.scrollTop)
+    console.log('message height', $newMessage.offsetHeight);
+    console.log('container Height', containerHeight)
+    console.log('scroll offset', scrollOffset)
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
+        username: message.username, 
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('locationMessage', (message) => {
     console.log(message)
     const html = Mustache.render(locationTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+
+    document.querySelector('#sidebar').innerHTML = html
+} )
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
